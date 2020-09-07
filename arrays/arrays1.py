5dea8ef6bbd92619034771d37ebdd885907f2478
LachlanVelden-swin <ldvandervelden@swin.edu.au>
2020-09-07 10:39:17 +1000

    added array code

---
+++ arrays/arrays1.py
@@ -0,0 +1,25 @@
+
+ThreeQueenStreet = ["Hey Bob, how are you today?", "Bills: Pay your rent.", 700]
+
+ThreeQueenStreet.append(10)
+
+ThreeQueenStreet.remove(ThreeQueenStreet[0])
+
+# position = 0
+# while(len(ThreeQueenStreet) > position):
+#     print(ThreeQueenStreet[position])
+#     position = position + 1
+
+for letter in ThreeQueenStreet:
+    print(letter)
+
+# print("-------------")
+# print("We have been payed. Centerlink for the win! We got paid this much: ")
+# print(ThreeQueenStreet[2])
+# print("-------------")
+# ThreeQueenStreet[0] = "Hi Frank, im great thanks, just got paid!"
+
+# print(ThreeQueenStreet[0])
+# print(ThreeQueenStreet[1])
+
+
